02c84a2aadf1b5eb185fddf3b7a5862644cfba41
Tom McKeesick <tmck01@gmail.com>
2020-05-07 21:50:51 +1000

    Some code refactor & tidy

--- beginner/lesson016/shapes.py
+++ beginner/lesson016/shapes.py
@@ -7,10 +7,6 @@ class Shape:
         self.width = width
         self.height = height
         self.pixel_states = pixel_states
-        self.__post_init__()
-
-    def __post_init__(self):
-        pass
 
     def should_fill(self, x, y):
         raise NotImplementedError('Must implement should_fill(self, x, y) method')
@@ -27,23 +23,25 @@ class Shape:
     def __str__(self):
         return f'{self.__class__.__name__}, coords: ' + list(self.draw())
 
+
 class FuzzyShape(Shape):
+
     def __init__(self, *args, tolerance=2, debug=False, **kwargs):
         super().__init__(*args, **kwargs)
         self.tolerance = tolerance
         self.debug = debug
-
-    def pixel_to_draw(self, x, y):
-        if self.debug:
-            return str(self.fill_calculation(x, y))
-        else:
-            return self.pixel_states.full
 
     def should_fill(self, x, y):
         return self.fill_calculation(x, y) < self.tolerance
 
     def fill_calculation(self, x, y):
         raise NotImplementedError('Must implement fill_calculation')
+
+    def pixel_to_draw(self, x, y):
+        if self.debug:
+            return str(self.fill_calculation(x, y))
+        else:
+            return self.pixel_states.full
 
 # Squares ---------------------------------------
 
@@ -59,19 +57,14 @@ class HollowSquare(Shape):
 # Circles ---------------------------------------
 
 class FilledCircle(FuzzyShape):
-    '''
-    https://www.purplemath.com/modules/sqrcircle.htm
-    The center-radius form of the circle equation is in the format
-        (x – h)2 + (y – k)2 = r2
-    where
-        (h,k) are the center coordinates, and r is the radius.
-    '''
+    '''(x – h)2 + (y – k)2 = r2
+       where (h,k) are the center coordinates, and r is the radius.'''
 
     def fill_calculation(self, x, y):
         h, k = int(self.width/2), int(self.height/2)
-        l = int((x-h)**2 + (y-k)**2)
-        r = int(self.width/2)**2
-        return l - r
+        r = int(self.width/2)
+        return int((x-h)**2 + (y-k)**2) - r**2
+
 
 class Exponential(FuzzyShape):
     def fill_calculation(self, x, y):
