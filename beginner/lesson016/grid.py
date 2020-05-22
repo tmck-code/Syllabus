bf50163ef4d55d2d6f529f52978a5071790d7f6a
Tom McKeesick <tmck01@gmail.com>
2020-05-22 23:16:41 +1000

    Tidy up grid

--- beginner/lesson016/grid.py
+++ beginner/lesson016/grid.py
@@ -1,48 +1,46 @@
 from dataclasses import dataclass
-from enum import Enum
 
 @dataclass
 class Pixels:
-    'Holds the two characters representing possible pixel state (full/empty)'
+    'The characters that represent each possible pixel state (full/empty)'
 
     full:  str = '▣'
     empty: str = '▢'
 
 
+@dataclass
 class CartesianGrid:
-    '''An x-y pixel grid. Optionally set padding width, and empty pixel that is used for all cells
-    in the 'blank' canvas'''
+    '''An x-y pixel grid. Optionally set padding width, and empty pixel'''
 
-    def __init__(self, width: int, height: int, padding=0, pixels=Pixels()):
-        self.width = width
-        self.height = height
-        self.padding = padding
-        self.pixels = pixels
+    width:   int
+    height:  int
+    padding: int = 0
+    empty_pixel: str = Pixels.empty
+
+    def __post_init__(self):
         self.grid = self.__create_grid()
 
-    def draw(self, *shapes, dx=0, dy=0, render=False):
-        '''Draws 1+ shapes on the canvas, with the shapes' starting coordinate optionally
-        translated by dx & dy. If render is True, returns the grid as a string'''
+    def draw(self, *shapes, dx=0, dy=0):
+        '''Draw shapes on the canvas, with the shapes' starting coordinate optionally
+        offset by (dx, dy).'''
 
-        # Calculate the x, y of the grid center
+        # Calculate (x, y) for the grid center
         h, k = int(self.width/2), int(self.height/2)
         for shape in shapes:
-            # Calculate the x, y of the top-left corner of the shape so that it is
-            # positioned in the centre of the grid
+            # Calculate (x, y) for the top-left corner of the shape, so that it is positioned in the
+            # centre of the grid
             cx, cy = h-int(shape.width/2)+dx, k-int(shape.height/2)+dy
             for p, q, pixel in shape.draw():
                 self.__fill_cell(x=p+cx, y=q+cy, pixel=pixel)
-        if render:
-            return self.__str__()
 
     def clear_grid(self):
-        'Sets the grid to its initial state, removing any drawn shapes'
+        'Set the grid to its initial state, removing any drawn shapes'
         self.grid = self.__create_grid()
 
     def __create_grid(self):
         grid = []
         for _ in range(0, self.height+(self.padding*2)):
-            grid.append([self.pixels.empty for _ in range(0, self.width+(self.padding*2))])
+            grid.append([self.empty_pixel for _ in range(0, self.width+(self.padding*2))])
         return grid
 
     def __fill_cell(self, x, y, pixel):
