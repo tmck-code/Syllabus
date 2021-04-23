2fa5a4b42512501442ff874dde1f5fcae1eaa0c4
Tom McKeesick <tmck01@gmail.com>
2021-04-23 15:13:22 +1000

    lesson progress

---
+++ intermediate/utils.py
@@ -0,0 +1,10 @@
+import json
+from pygments import highlight
+from pygments.lexers import JsonLexer
+from pygments.formatters import TerminalTrueColorFormatter
+def ppj(j):
+    print(highlight(
+        json.dumps(j, indent=2),
+        JsonLexer(),
+        TerminalTrueColorFormatter(),
+    ))
