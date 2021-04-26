18b3b4f54b6e2d58bde4bb54003e3b7589b318f6
Tom McKeesick <tmck01@gmail.com>
2021-04-26 22:22:16 +1000

    update

--- intermediate/lesson02_classes/utils.py
+++ intermediate/lesson02_classes/utils.py
@@ -2,9 +2,13 @@ import json
 from pygments import highlight
 from pygments.lexers import JsonLexer
 from pygments.formatters import TerminalTrueColorFormatter
+
 def ppj(j):
     print(highlight(
         j,
         JsonLexer(),
         TerminalTrueColorFormatter(),
     ))
+   
+def ppd(d, indent=2):
+    ppj(json.dumps(d, indent=indent))
