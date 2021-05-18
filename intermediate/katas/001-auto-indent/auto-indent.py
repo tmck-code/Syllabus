0c383e130d6462362b21c6fbc738d8a43045a83b
Tom McKeesick <tmck01@gmail.com>
2021-05-18 21:42:42 +1000

    lol

--- intermediate/katas/001-auto-indent/auto-indent.py
+++ intermediate/katas/001-auto-indent/auto-indent.py
@@ -16,27 +16,47 @@ def __rec(raw: dict, indent=2, level=0) -> str:
 def __inc(raw: dict, indent=2) -> str:
     inc_level = {'{'}
     dec_level = {'}'}
-    sdec_level = [']']
+    sdec_level = {'}', ']'}
     level = 0
     pretty = ''
     i = iter(json.dumps(raw))
     for ch in i:
+        if ch == ',':
+            ch2 = next(i)
+            if ch2 == ' ':
+                pretty += ','
+                continue
+        # Put the char, then newline & increase indent
         if ch in inc_level:
-            level += 1
+            level += indent
             pretty += ch
             pretty += '\n' + ' '*level
+        # Put a newline & down a level, then put current char
         elif ch in dec_level:
-            level -=1
+            level -= indent
             pretty += '\n' + ' '*level
             pretty += ch
+        # If we are at a ]
         elif ch in sdec_level:
-            ch+=next(i)
-            level -=1
-            pretty += ch
-            pretty += '\n' + ' '*level
+            ch2=next(i)
+            # Check if it's a comma next, i.e. "],"
+            if ch2 == ',':
+                # Then, put the "]," onto the current line, then newline, no
+                # level change
+                pretty += ch
+                pretty += ch2
+                pretty += '\n' + ' '*level
+            # Otherwise, it's another level char like "]]" or "]}"
+            else:
+                # Put the current char, then newline & down a level for the next
+                # level char
+                pretty += ch
+                level -= indent
+                pretty += '\n' + ' '*level
+                pretty += ch2
         else:
             pretty += ch
-    return pretty
+    return pretty + '\n'
 
 def pretty_json(raw: dict, indent=2) -> str:
     'Returns an auto-indented/pretty JSON string'
@@ -54,9 +74,13 @@ tests = [
     ),
 ]
 
-for i, ((data, indent), expected) in enumerate(tests):
-    print(i, expected, sep='\n')
+# for i, ((data, indent), expected) in enumerate(tests):
+#     print(i, expected, sep='\n')
 
 for (data, indent), expected in tests:
     result = pretty_json(data, indent)
+    print('result\n', result)
+    print('expected\n', expected)
+    for l1, l2 in zip(expected.split('\n'), result.split('\n')):
+        assert l1 == l2, f'{l1} != {l2}'
     assert result == expected, f'{result} != {expected}'
