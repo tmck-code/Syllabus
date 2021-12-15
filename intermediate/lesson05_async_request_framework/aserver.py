355a5b915793588079a4086eac612089dcfc7726
Tom McKeesick <tmck01@gmail.com>
2021-12-16 01:01:34 +1100

    init lesson 05

---
+++ intermediate/lesson05_async_request_framework/aserver.py
@@ -0,0 +1,98 @@
+from aiohttp import web
+
+import asyncio
+import json
+from dataclasses import dataclass
+import time
+import functools
+
+class RateLimitError(Exception): pass
+
+@dataclass
+class Rater:
+    max_requests: int
+    time_window: int
+
+    def __post_init__(self):
+        self.start_time = time.time()
+        self.total = 0
+
+    def __time_remaining(self):
+        return 60-(time.time()-self.start_time)
+
+    def inc(self):
+        # If we are over the time window, reset counter & time
+        if (time.time() - self.start_time) > 60:
+            self.total = 0
+            self.start_time = time.time()
+        # Now, increment the total
+        self.total += 1
+        # If there are too many requests, raise error
+        if self.total > self.max_requests:
+            raise RateLimitError(json.dumps({
+                "limit": self.rate_limit,
+                "current": self.total,
+                "remaining": self.__time_remaining()
+            }))
+    
+def with_rater(func):
+    @functools.wraps(func)
+    async def wrapped(*args, **kwargs):
+        print(f"Request to '{func.__name__}': {args[1]}")
+
+        args[0].rate_limit.inc()
+        return await func(*args, **kwargs)
+    return wrapped
+
+
+
+@dataclass
+class API:
+    rate_limit: Rater
+    max_items: int = 100
+
+    def __post_init__(self):
+        self.last_request = time.time()
+
+    @with_rater
+    async def get_root(self, request):
+        return web.Response(text="Hello, world\n")
+
+    @with_rater
+    async def get_items(self, request):
+        return web.Response(text=json.dumps(list(range(100))))
+
+
+    def rate_limit(func):
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs):
+
+            cls = args[0]
+            elapsed = time.time()-cls.last_request
+
+            print("elapsed", elapsed)
+            sleep_time = 0
+            if elapsed < cls.rate_limit:
+                sleep_time = cls.rate_limit - elapsed
+            print("sleeping for", sleep_time)
+            await asyncio.sleep(sleep_time)
+
+            print("rate limit released")
+            cls.last_request = time.time()
+
+            return await func(*args, **kwargs)
+        return wrapped
+
+def run():
+    app = web.Application()
+    api = API(rate_limit=Rater(60, 60), max_items=100)
+    app.add_routes(
+        [
+            web.get('/', api.get_root),
+            web.get('/items', api.get_items),
+        ]
+    )
+    web.run_app(app)
+
+if __name__ == "__main__":
+    run()
