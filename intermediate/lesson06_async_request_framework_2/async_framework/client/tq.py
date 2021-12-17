3ae9fcc59e133b5f7860b9b4afb2254dd5b6392b
Tom McKeesick <tmck01@gmail.com>
2021-12-17 16:08:32 +1100

    init lesson 06

---
+++ intermediate/lesson06_async_request_framework_2/async_framework/client/tq.py
@@ -0,0 +1,156 @@
+class ThrottledQueue(asyncio.Queue):
+    "subclass asyncio.Queue i.e. import all behaviour"
+
+    def __init__(self, per_second, debug=False, maxsize=0, *, loop=None, i=0):
+        "Set up some extra vars and then call the original init"
+
+        self.lock = asyncio.Lock()
+        self.i = i
+        self.per_second = per_second
+        self.last_get = time.perf_counter_ns() # this is the fastest way... I think?
+        self.debug = debug
+        super(ThrottledQueue, self).__init__(maxsize=maxsize, loop=loop)
+
+    async def notify(self, override: int):
+        """
+        Signals to the queue that an item is being retried,
+        so pause any get()s by aquiring the lock and throttling before releasing
+        """
+        async with self.lock:
+            await self._throttle(override)
+
+    async def get(self):
+        async with self.lock:
+            await self._throttle()
+            result = await super(ThrottledQueue, self).get()
+
+            self.last_get = time.perf_counter_ns()
+            return result
+
+    async def _throttle(self, override: int=0):
+        if override > 0:
+            await asyncio.sleep(override)
+            return
+        elapsed = time.time() - self.last_get
+        sleep_time = (1/float(self.per_second)) - elapsed
+        if self.debug:
+            print(self.i, '- times', f'{elapsed:.5f}', '+', f'{sleep_time:.5f}', '=', self.per_second, '- sizes', self.qsize(), f'{self.qsize() / max(1, self.maxsize):.5f}')
+        await asyncio.sleep(max(0, sleep_time)) # Make sure we wait at least 0 seconds
+
+
+t = asyncio.run(_fill_queue(ThrottledQueue(per_second=1), [("get", "0.0.0.0", "8080", "items")]))
+t2 = ThrottledQueue(per_second=100)
+res = asyncio.Queue()
+
+all_customers_req = AsyncRequester(
+    in_q          = t,
+    out_q         = t2,
+    req_builder   = base_request_builder,
+    resp_unpacker = base_response_unpacker,
+    error_handler = handle_error,
+    log_prefix    = "+++",
+)
+
+customers_by_id_req = AsyncRequester(
+    in_q          = t2,
+    out_q         = res,
+    req_builder   = id_request_builder,
+    resp_unpacker = id_response_unpacker,
+    error_handler = handle_error,
+    log_prefix    = "___",
+)
+
+asyncio.run(
+    asyncio.gather(
+        asyncio.create_task(all_customers_req.consumer(0)),
+        asyncio.create_task(customers_by_id_req.consumer(0)),
+        asyncio.create_task(customers_by_id_req.consumer(1)),
+    )
+)
+
+
+@dataclass
+class AsyncRequester:
+    in_q: ThrottledQueue
+    out_q: ThrottledQueue
+    req_builder: object
+    resp_unpacker: object
+    error_handler: object
+    log_prefix: str = "---"
+    cntr: Counter = field(default_factory=Counter)
+
+    async def consumer(self, idx):
+        retrying = False
+        while True:
+            if not retrying:
+                i, d = await self.in_q.get()
+                print(self.log_prefix, d)
+                if d == Sentinel:
+                    await self.in_q.put((i, Sentinel))
+                    await self.out_q.put((i, Sentinel))
+                    print(self.log_prefix, f"worker {idx} exiting")
+                    return
+            async with request(*self.req_builder(*d)) as req:
+                resp = await req.read()
+                try:
+                    req.raise_for_status()
+                except ClientResponseError as e:
+                    self.cntr["failure"] += 1
+                    await error_handler(e, q)
+                    retrying = True
+                    # TODO implement retry limit
+                    continue
+                print(self.log_prefix, f"worker {idx} response for #{i}: {resp}")
+                print(self.log_prefix, f"sending to queue: {resp}")
+                await self.resp_unpacker(d, resp, self.out_q)
+                self.cntr["success"] += 1
+
+
+import asyncio
+from itertools import count
+
+async def _fill_queue(q, items):
+    for idx, i in enumerate(items):
+        await q.put((idx, i))
+    await q.put((idx, Sentinel))
+    return q
+
+async def _unpack_queue(q):
+    l = list()
+    for idx in range(q.qsize()):
+        l.append(await q.get())
+    return l
+
+def unpack_queue(q):
+    return asyncio.run(_unpack_queue(q))
+
+
+import json
+
+from collections import Counter
+
+from dataclasses import field, dataclass
+import sys
+from aiohttp import ClientResponseError
+
+class Sentinel: pass
+
+async def id_response_unpacker(req_data, resp, queue, *args):
+    await queue.put((0, (resp,)))
+
+async def base_response_unpacker(req_data, resp, queue):
+    for i in json.loads(resp):
+        await queue.put((0, (*req_data, i)))
+
+def id_request_builder(method, hostname, port, endpoint, i):
+    return (method, f"http://{hostname}:{port}/{endpoint}/{i}")
+
+def base_request_builder(method, hostname, port, endpoint):
+    return (method, f"http://{hostname}:{port}/{endpoint}")
+
+async def handle_error(e, q):
+    print(f"HANDLING ERROR: {e}")
+    if e.status == 429:
+        q.retry()
+    elif e.status == 503:
+        q.lock(10)
---
+++ intermediate/lesson06_async_request_framework_2/async_framework/server/aserver.py
@@ -0,0 +1,93 @@
+from aiohttp import web
+
+import json
+from dataclasses import dataclass
+import time
+import functools
+from faker import Faker
+
+class RateLimitError(Exception):
+    def __init__(self, *args, info: dict = {}, **kwargs):
+        self.info = info
+        super().__init__(*args, **kwargs)
+
+@dataclass
+class Rater:
+    max_requests: int
+    time_window: int = 60
+
+    def __post_init__(self):
+        self.start_time = time.time()
+        self.total = 0
+
+    def __time_remaining(self):
+        return self.time_window-(time.time()-self.start_time)
+
+    def inc(self):
+        # If we are over the time window, reset counter & time
+        if (time.time() - self.start_time) > self.time_window:
+            self.total = 0
+            self.start_time = time.time()
+        # Now, increment the total
+        self.total += 1
+        # If there are too many requests, raise error
+        if self.total > self.max_requests:
+            raise RateLimitError(
+                "Exceeded rate limit",
+                info={
+                    "limit": self.max_requests,
+                    "current": self.total,
+                    "remaining": self.__time_remaining()
+                }
+            )
+    
+def with_rater(func):
+    @functools.wraps(func)
+    async def wrapped(*args, **kwargs):
+        print(f"Request to '{func.__name__}': {args[1]}")
+        try:
+            args[0].rate_limit.inc()
+        except RateLimitError as e:
+            print(f"Rate limiting!", e.info)
+            return web.Response(text=json.dumps({"error": e.info}), status=429)
+        return await func(*args, **kwargs)
+    return wrapped
+
+
+@dataclass
+class API:
+    rate_limit: Rater
+    max_items: int = 100
+
+    def __post_init__(self):
+        self.last_request = time.time()
+        self.faker = Faker()
+        self.items = [self.faker.email() for _ in range(self.max_items)]
+
+    @with_rater
+    async def get_root(self, request):
+        return web.Response(text="Hello, world\n")
+
+    @with_rater
+    async def get_items(self, request):
+        return web.Response(text=json.dumps(list(range(self.max_items))))
+
+    @with_rater
+    async def get_item(self, request):
+        idx = int(request.match_info["id"])
+        return web.Response(text=json.dumps({"email": self.items[idx]}))
+
+def run(rate_limit: Rater, max_items: int):
+    api = API(rate_limit=rate_limit, max_items=max_items)
+    app = web.Application()
+    app.add_routes(
+        [
+            web.get('/',      api.get_root),
+            web.get('/items', api.get_items),
+            web.get('/items/{id}', api.get_item),
+        ]
+    )
+    web.run_app(app)
+
+if __name__ == "__main__":
+    run(rate_limit=Rater(6_000, 60), max_items=1_000)
