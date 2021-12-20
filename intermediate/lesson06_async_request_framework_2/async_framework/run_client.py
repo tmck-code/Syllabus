8c4034638f5423f171ba50dbe0458d9ce792f671
Tom McKeesick <tmck01@gmail.com>
2021-12-20 21:40:29 +1100

    Make unpacker & throttledworker

--- intermediate/lesson06_async_request_framework_2/async_framework/run_client.py
+++ intermediate/lesson06_async_request_framework_2/async_framework/run_client.py
@@ -10,7 +10,42 @@ from typing import List
 import time
 import random
 
-from lib.arequests import AsyncEndpointPipeline, AsyncEndpoint, unpack_queue, Sentinel
+from lib.arequests import AsyncEndpointPipeline, AsyncEndpoint, ThrottledQueue, unpack_queue, Sentinel
+from abc import abstractmethod
+@dataclass
+class Unpacker:
+    "gets QueueItems from an asyncio.Queue, unpacks to another QueueItem, puts on a ThrottledQueue"
+
+    in_q: asyncio.Queue
+    out_q: ThrottledQueue
+
+    @abstractmethod
+    async def unpack(self, d: object) -> object:
+        "Unpacks a queue object/transforms it for a ThrottledWorker"
+
+    async def run(self):
+        while True:
+            d = await self.in_q.get()
+            for el in self.unpack(d):
+                await self.out_q.put(el)
+
+
+@dataclass
+class ThrottledWorker:
+    "gets QueueItems from a ThrottledQueue, builds into a requests, awaits response and puts on a asyncio.Queue"
+
+    in_q: ThrottledQueue
+    out_q: asyncio.Queue
+
+    @abstractmethod
+    async def work(self, d):
+        "Does some work on a QueueItem and returns the result"
+
+    async def run(self):
+        while True:
+            d = await self.in_q.get()
+            resp = await self.work(d)
+            await self.out_q.put(d)
 
 
 @dataclass
